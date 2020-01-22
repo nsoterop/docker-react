4067fd5de13a2b78cfd1484a4738cf5588126df6
Nick <nsoterop@gmail.com>
2020-01-22 15:55:44 -0500

    Modified travis file

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
 import ReactDOM from 'react-dom';
+import App from './App';
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
